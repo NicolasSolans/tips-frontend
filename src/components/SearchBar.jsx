177b43173544efade01462fdfb86b7ec9e4bb654
NicolasSolans <nico.solans.drc@gmail.com>
2023-11-08 13:40:32 -0300

    Pequeños cambios de estilos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,5 @@
+import { SearchIcon } from '@heroicons/react/solid';
+
 export const SearchBar = (props) => {
     const {handleSubmit, handleChange} = props;
   return (
@@ -5,8 +7,9 @@ export const SearchBar = (props) => {
         <form onChange={(event) => handleChange(event)}>
 
         <label className="label">
-        <input type="search" placeholder="Buscar productos" className="input input-bordered input-info w-full max-w-xs" />
-        <button onClick={(event) => handleSubmit(event)} type="submit" className="btn btn-neutral">Buscar</button>
+        <input type="search" className="input input-bordered w-full max-w-xs" />
+        <button onClick={(event) => handleSubmit(event)} type="submit" className="btn btn-active btn-neutral">
+          <SearchIcon className="w-5 h-5 mr-2"/>  <span className='normal-case'>Buscar</span></button>
         </label>
         </form>
     </div>
